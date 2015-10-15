02e347dffd0ca14ca5dfc23285e24e2107d51782
Tom Barrett <spalf0@gmail.com>
2015-10-15 00:40:06 -0500

    Create status.sh

---
+++ i3/status.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# shell script to prepend i3status with more stuff
+
+i3status | while :
+do
+	read line
+	MPCSTAT=`mpc | grep -v '^volume:'`
+	if [ -z "${MPCSTAT}" ]; then
+		echo "$line" || exit 1
+	else
+		MPCSTAT=`echo "${MPCSTAT}" | sed 's,  *, ,g; 1h;1d;2G' | paste -d' ' -s -`
+		echo "${MPCSTAT} | $line" || exit 1
+	fi
+done
+
+
