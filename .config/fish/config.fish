861f1c2c4553663dd4b46fbaabe73d0df5ba8602
Tom Barrett <tom@tombarrett.xyz>
2020-01-27 04:35:21 -0600

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-set PATH /bin /home/tom/.cargo/bin /sbin /usr/bin /usr/games /usr/local/bin /usr/local/games /usr/local/sbin /usr/sbin
+set PATH /bin /home/tom/.cargo/bin /sbin /usr/bin /usr/games /usr/local/bin /usr/local/games /usr/local/sbin /usr/sbin /home/tom/bin
 
 function fish_user_key_bindings
 	for mode in insert default visual
