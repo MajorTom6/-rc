1e13cfa09d1a2c057a7a4b9590daf569bd91b760
Tom Barrett <tom@tombarrett.xyz>
2020-03-09 07:52:31 -0500

    new fish settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,17 @@
-set PATH /bin /home/tom/.cargo/bin /sbin /usr/bin /usr/games /usr/local/bin /usr/local/games /usr/local/sbin /usr/sbin /home/tom/bin
+set PATH /bin /sbin /usr/bin /usr/games \
+	 /usr/local/bin /usr/local/games \
+	 /usr/local/sbin /usr/sbin \
+	 /home/tom/bin \
+	 /home/tom/.cargo/bin \
+	 /home/tom/.local/bin
+
+set GOPATH /home/tom/src/go
 
 function fish_user_key_bindings
 	for mode in insert default visual
 		bind -M $mode \cf forward-char
 	end
+end
+
+function fish_mode_prompt
 end
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,42 +1,24 @@
 function fish_prompt --description 'Write out the prompt'
-	
-    set stat $status
+	if set -l branch_name (command git symbolic-ref HEAD 2>/dev/null | string replace refs/heads/ '')
+		set git_info (set_color d8d8d8)"|"
+		if command git diff-index --quiet HEAD --
+			set git_info $git_info(set_color f7ca88)
+		else
+			set git_info $git_info(set_color ab4642)
+		end
+		set git_info $git_info"$branch_name"
+	end
 
-    if not set -q __fish_prompt_normal
-        set -g __fish_prompt_normal (set_color normal)
-    end
+	# Disable PWD shortening by default.
+	set -q fish_prompt_pwd_dir_length
+	or set -lx fish_prompt_pwd_dir_length 0
 
-    if not set -q __fish_color_blue
-        set -g __fish_color_blue (set_color -o blue)
-    end
-
-    #Set the color for the status depending on the value
-    set __fish_color_status (set_color -o green)
-    if test $stat -gt 0
-        set __fish_color_status (set_color -o red)
-    end
-
-    switch "$USER"
-
-        case root toor
-
-            if not set -q __fish_prompt_cwd
-                if set -q fish_color_cwd_root
-                    set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
-                else
-                    set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-                end
-            end
-
-            printf '%s@%s %s%s%s# ' $USER (prompt_hostname) "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal"
-
-        case '*'
-
-            if not set -q __fish_prompt_cwd
-                set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-            end
-
-            printf '[%s] %s%s@%s %s%s %s(%s)%s \f\r> ' (date "+%H:%M:%S") "$__fish_color_blue" $USER (prompt_hostname) "$__fish_prompt_cwd" "$PWD" "$__fish_color_status" "$stat" "$__fish_prompt_normal"
-
-    end
+	printf '%s' (set_color -o d8d8d8) '<'
+	printf '%s' (set_color a1b56c) $USER
+	printf '%s' (set_color d8d8d8) '@'
+	printf '%s' (set_color 7cafc2) (prompt_hostname)
+	printf '%s' (set_color d8d8d8) '|'
+	printf '%s' (set_color ba8baf) (prompt_pwd)
+	printf '%s' $git_info
+	printf '%s' (set_color d8d8d8) '>'
 end
