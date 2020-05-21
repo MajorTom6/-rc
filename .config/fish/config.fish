704076e70a8ec9e4e5463368fa20731a1a7a91b0
Tom Barrett <tom@tombarrett.xyz>
2020-05-21 12:16:50 +0200

    give my directory highest prio

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
-set PATH /bin /sbin /usr/bin /usr/games \
+set PATH \
+	 /home/tom/bin \
+	 /bin /sbin /usr/bin /usr/games \
 	 /usr/local/bin /usr/local/games \
 	 /usr/local/sbin /usr/sbin \
-	 /home/tom/bin \
 	 /home/tom/.cargo/bin \
 	 /home/tom/.local/bin
 
